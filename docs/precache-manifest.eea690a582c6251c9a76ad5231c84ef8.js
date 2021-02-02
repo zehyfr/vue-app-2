3128a1147c3b97831d171dbe561bb1c13c52fc78
zehyfr <zehyfrab@gmail.com>
2021-02-02 15:17:15 +0100

    Building for Github Pages

---
+++ docs/precache-manifest.eea690a582c6251c9a76ad5231c84ef8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd9e3092e598c96b5c1",
+    "url": "/vue-app-2/css/app.ddeedaa8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "75ad65ef27f48b8fc32c2a2d17d9fe4a",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "1a96fce5b1b624bf999d",
+    "url": "/vue-app-2/js/about.0d0ec44c.js"
+  },
+  {
+    "revision": "fcd9e3092e598c96b5c1",
+    "url": "/vue-app-2/js/app.3d227d0d.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
